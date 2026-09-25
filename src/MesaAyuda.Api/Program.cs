b10ff5d8d534a885a1f2848254f6cef36bf61df2
Juan Rosario <juan.rodriguez@idac.gov.do>
2026-09-25 15:11:09 -0400

    Registra el repositorio JSON al iniciar

--- src/MesaAyuda.Api/Program.cs
+++ src/MesaAyuda.Api/Program.cs
@@ -1,6 +1,14 @@
+using MesaAyuda.Api.Persistencia;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+// La ruta del archivo se puede cambiar con la variable de entorno Almacenamiento__RutaDatos.
+var almacenamiento = builder.Configuration.GetSection(OpcionesAlmacenamiento.Seccion).Get<OpcionesAlmacenamiento>()
+    ?? new OpcionesAlmacenamiento();
+var rutaDatos = Path.Combine(builder.Environment.ContentRootPath, almacenamiento.RutaDatos);
+builder.Services.AddSingleton<IRepositorioTickets>(new RepositorioTicketsJson(rutaDatos));
 
 var app = builder.Build();
 
