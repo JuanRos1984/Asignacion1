aa91dd4b83104dd322c478b5d459ea9dc1a29050
Juan Rosario <juan.rodriguez@idac.gov.do>
2026-09-25 15:09:13 -0400

    Crea el esqueleto de la Web API

---
+++ src/MesaAyuda.Api/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapGet("/salud", () => Results.Ok(new { estado = "ok" }));
+
+app.Run();
